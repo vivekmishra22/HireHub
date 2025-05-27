dbaa5aed23c9db6964a7c5e0b3bb2d51a6355fd9
Vivek Mishra <vivekunofficialmail@gmail.com>
2025-05-27 23:55:27 +0530

    Model Created

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export const resister = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+// const express = require('express'); old way
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
+
+const app = express();
+
+// app.get("/home", (req, res) => {
+//     return res.status(200).json({
+//         message: "I'm comming from backend",
+//         success:true
+//     })
+// });
+
+// mongodb+srv://vivekunofficialmail:pX4ndqR3W5neU4sN@cluster0.l8ygd4s.mongodb.net/
+
+// middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+});
