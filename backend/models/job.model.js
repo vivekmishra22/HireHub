3aac01df74442c986550686ac2d004ed7bcf6da0
Vivek Mishra <vivekunofficialmail@gmail.com>
2025-05-31 20:51:07 +0530

    application controller and route added successfully

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -42,10 +42,12 @@ const jobSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
-    applications: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Application',
-    }
+    applications: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application',
+        }
+    ]
 }, { timestamps: true });
 
 export const Job = mongoose.model('Job', jobSchema);
