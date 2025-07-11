3f90c3b8e45176bc3786e1103031e557a36eac9d
Vivek Mishra <vivekunofficialmail@gmail.com>
2025-07-11 23:38:51 +0530

    ApplicantsTable done and also few changes in application route

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.route("/apply/:id").get(isAuthenticated, applyJob);
 router.route("/get").get(isAuthenticated, getAppliedJobs);
 router.route("/:id/applicants").get(isAuthenticated, getApplicants);
-router.route("/status/:id/update").put(isAuthenticated, updateStatus);
+router.route("/status/:id/update").post(isAuthenticated, updateStatus);
 
 export default router;
--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -3,12 +3,27 @@ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Tabl
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { MoreHorizontal } from 'lucide-react';
 import { useSelector } from 'react-redux';
+import { toast } from 'sonner';
+import axios from 'axios';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
 
 const shortlistingStatus = ["Accepted", "Rejected"];
 
 const ApplicantsTable = () => {
 
     const { applicants } = useSelector(store => store.application);
+
+    const statusHandler = async (status, id) => {
+        try {
+            axios.defaults.withCredentials = true;
+            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, { status });
+            if(res.data.success){
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
 
     return (
         <div>
@@ -31,11 +46,11 @@ const ApplicantsTable = () => {
                                 <TableCell>{item?.applicant?.fullname}</TableCell>
                                 <TableCell>{item?.applicant?.email}</TableCell>
                                 <TableCell>{item?.applicant?.phoneNumber}</TableCell>
-                                <TableCell> 
+                                <TableCell>
                                     {
-                                        item?.applicant?.profile?.resume ? 
-                                        <a className='text-blue-600 cursor-pointer' href={item?.applicant?.profile?.resume} target='_blank' rel='noopener noreferrer'>{item?.applicant?.profile?.resumeOriginalName}</a> :
-                                        <span>NA</span>
+                                        item?.applicant?.profile?.resume ?
+                                            <a className='text-blue-600 cursor-pointer' href={item?.applicant?.profile?.resume} target='_blank' rel='noopener noreferrer'>{item?.applicant?.profile?.resumeOriginalName}</a> :
+                                            <span>NA</span>
                                     }
                                 </TableCell>
                                 {/* <TableCell>{item?.applicant?.profile?.resumeOriginalName}</TableCell> */}
@@ -49,7 +64,7 @@ const ApplicantsTable = () => {
                                             {
                                                 shortlistingStatus.map((status, index) => {
                                                     return (
-                                                        <div key={index} className='flex w-fit items-center my-2 cursor-pointer'>
+                                                        <div onClick={() => statusHandler(status, item?._id)} key={index} className='flex w-fit items-center my-2 cursor-pointer'>
                                                             <span>{status}</span>
                                                         </div>
                                                     )
