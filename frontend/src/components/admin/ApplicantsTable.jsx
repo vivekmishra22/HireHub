94962e4197c8510c508eedd64d1b5b7c4fa9bdbe
Vivek Mishra <vivekunofficialmail@gmail.com>
2025-07-26 14:13:23 +0530

    comment added to ApplicantsTable inside admin folder

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
-import { MoreHorizontal } from 'lucide-react';
-import { useSelector } from 'react-redux';
-import { toast } from 'sonner';
-import axios from 'axios';
-import { APPLICATION_API_END_POINT } from '@/utils/constant';
+import React from 'react'       // Importing necessary modules and components
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'    // Custom UI table components
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';        // Popover for dropdown actions
+import { MoreHorizontal } from 'lucide-react';      // Icon with 3 dots (⋯)
+import { useSelector } from 'react-redux';          // To get Redux state
+import { toast } from 'sonner';                     // Toast notification library
+import axios from 'axios';                          // Axios for HTTP requests
+import { APPLICATION_API_END_POINT } from '@/utils/constant';       // API endpoint constant
 
-const shortlistingStatus = ["Accepted", "Rejected"];
+const shortlistingStatus = ["Accepted", "Rejected"];        // Possible shortlisting statuses for an applicant
 
-const ApplicantsTable = () => {
+const ApplicantsTable = () => {         // Main component
 
-    const { applicants } = useSelector(store => store.application);
+    const { applicants } = useSelector(store => store.application);     // Accessing applicants data from Redux store
 
-    const statusHandler = async (status, id) => {
+    const statusHandler = async (status, id) => {                       // Handler function to update applicant status (Accepted / Rejected)
         try {
-            axios.defaults.withCredentials = true;
-            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, { status });
-            if(res.data.success){
+            axios.defaults.withCredentials = true;                      // Allow cookies/session on request
+            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, { status });       // API call to update status
+            if(res.data.success){                   // If request is successful, show success toast
                 toast.success(res.data.message);
             }
-        } catch (error) {
+        } catch (error) {           // If there's an error, show error toast
             toast.error(error.response.data.message);
         }
     }
@@ -28,7 +28,9 @@ const ApplicantsTable = () => {
     return (
         <div>
             <Table>
+                {/* Table caption (optional) */}
                 <TableCaption>A list of your recent applied user</TableCaption>
+                {/* Table header row */}
                 <TableHeader>
                     <TableRow>
                         <TableHead>Full Name</TableHead>
@@ -41,6 +43,7 @@ const ApplicantsTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
+                        // Mapping each application to a row
                         applicants && applicants?.applications?.map((item) => (
                             <tr key={item._id}>
                                 <TableCell>{item?.applicant?.fullname}</TableCell>
@@ -54,6 +57,7 @@ const ApplicantsTable = () => {
                                     }
                                 </TableCell>
                                 {/* <TableCell>{item?.applicant?.profile?.resumeOriginalName}</TableCell> */}
+                                {/* Application submission date (formatted to only show date, not time) */}
                                 <TableCell>{item?.applicant?.createdAt.split('T')[0]}</TableCell>
                                 <TableCell className={'float-right cursor-pointer'}>
                                     <Popover>
